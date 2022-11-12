b904d12ddfb45cd5ad4dae92cc578ba621c5528d
Couleur <82747632+couleurm@users.noreply.github.com>
2022-11-12 11:38:16 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
+> **Warning**
+>
+> This is not fully functional yet (e.g FFmpeg export option still missing) and still has a few rough edges
+
+Lets you set timestamps (which get represented for you by chapters) to export your videos with FFmpeg or [Smoothie](https://github.com/couleur-tweak-tips/Smoothie)
+
+### Keybinds
+
+* <kbd>g</kbd> and <kbd>h</kbd> to set the start and end points of your index (will be put at your current time position in the video)
+* <kbd>G</kbd> and <kbd>H</kbd> will do the same but set them at the very start of the video (00:00:00) or the very last second of the video
+
+* <kbd>CTRL+k</kbd> toggles between TRIM and SPLIT mode:
+    * SPLIT separates every single cut into separate files
+    * TRIM merges and joins the multiple cuts you have per video
+* <kbd>CTRL+v</kbd> toggles on/off verbose
+
+* <kbd>CTRL+e</kbd> toggles exporting modes:
+    * FFmpeg with keyframe cutting
+    * [Smoothie](https://github.com/couleur-tweak-tips/Smoothie), my VapourSynth-based motion blur program
+        * Default behavior when the script inits can be set in the first few lines of the lua script, when you are [installing Smoothie](https://github.com/couleur-tweak-tips/TweakList/blob/master/modules/Installers/Invoke-SmoothiePost.ps1) the default gets changed to Smoothie
+
+* <kbd>c</kbd> and <kbd>C</kbd> lets you go down (uncapped), and up (with capitalized) your **indexes**, which is what each `start`-`end` combo is called
+
+Or if you have the amazing [uosc](https://github.com/tomasklaen/uosc) ui installed you can press <kbd>Ctrl+t</kbd> for a beautiful index selector
+
+
+![](https://media.discordapp.net/attachments/829078609465180170/1040925209240272997/image.png)
+
+## Roadmap
+
+* Export option to FFmpeg (Lossless-Cut style)
+* Demo video
+
+### input.conf template (set to defaults)
+```yaml
 # suckless-cut
-Turn your mpv (video-player) into a bloat-free LosslessCut clone, using just a few hundred lines of Lua code ✨
+g       script-binding set-start
+G       script-binding set-sof
+h       script-binding set-fin
+H       script-binding set-eof
+Ctrl+r  script-binding exportSLC
+Ctrl+k  script-binding toggleSLCexportModes
+Ctrl+v  script-binding toggleSLCverbose
+Ctrl+g  script-binding getCurrentIndex
+C       script-binding increaseIndex
+c       script-binding decreaseIndex
+Ctrl+p  script-binding showPoints
+Ctrl+t  script-binding selectindex
+
+# misc suckless cut
+Ctrl+p  script-binding showPoints
+n       script-binding createChapter
+Ctrl+D  script-binding deletechapters
+R       script-binding reloadTrs
+```
---
+++ suckless-cut.lua
@@ -0,0 +1,392 @@
+
+local dur = 2500
+	-- Duration of OSC (top left) messages, in milliseconds,
+
+local verbose = false
+local mode = 'trim'
+	-- defaults, feel free to change them to whatever you need
+
+local mp = require 'mp'
+local msg = require 'mp.msg'
+local utils = require 'mp.utils'
+local options = require 'mp.options'
+
+Osdwarn = false -- Used when user made too many trimming points
+Trs = {} -- Creates a fresh empty table, will be appended start/fin timecodes
+Index = 1 -- Selects the trim 1, for it to be increased/decreased later
+local direcseparator = package.config:sub(1, 1) -- Returns / on *nix and \ on Windows
+
+local function verb(...)
+	local args = {...}
+	local text = ""
+
+	for i, v in ipairs(args) do
+	text = text .. tostring(v)
+	end
+	if verbose == true then
+		print("VERB", text)
+	end
+end
+
+
+
+local function notify(duration, ...)
+	local args = {...}
+	local text = ""
+
+	for i, v in ipairs(args) do
+	text = text .. tostring(v)
+	end
+
+	if text == nil then
+		return
+	end
+
+	print("", text)
+	mp.command(string.format("show-text \"%s\" %d 1", text, duration))
+end
+
+
+
+local function dump()
+	local vals = {
+		'container-fps','time-pos',
+		'playback-time/full', 'stream-open-filename',
+		'estimated-frame-number', 'duration'
+	}
+	for _, val in ipairs(vals) do
+		print(val, ": ", mp.get_property(val))
+	end
+end;mp.add_key_binding("Ctrl+d", "dump", dump)
+
+
+
+local function setindex (index)
+	notify(dur, ("Setting index to [" .. index .. "]"))
+	Index = index
+end
+mp.register_script_message('setindex', setindex)
+
+
+
+local function round(int)
+	return (math.floor(int * 100))/100
+end
+
+
+local function basename (filepath)
+	local t={}
+	for str in string.gmatch(filepath, "([^"..direcseparator.."]+)") do
+	 table.insert(t, str)
+	end
+	return t[#t]
+end
+
+
+
+
+local function selectindex ()
+	local fps = mp.get_property('container-fps')
+
+	local utils = require('mp.utils')
+	local menu = {
+	type = 'menu_type',
+	title = 'Select an index to switch back to',
+	items = {}
+	}
+	for i, cur in ipairs(Trs) do
+	if cur['start'] or cur['end'] then
+		menu['items'][i] = {}					     --/fps                                --/fps
+		menu['items'][i]['title'] = (round(cur['start']) or '') .. ' - ' .. (round(cur['fin']) or '')
+		menu['items'][i]['value'] = "script-message setindex " .. i
+		menu['items'][i]['hint'] = basename(cur['path'])
+	end
+	end
+
+	mp.commandv('script-message-to', 'uosc', 'open-menu', (utils.format_json(menu)))
+
+end;mp.add_key_binding("Ctrl+t", "selectindex", selectindex)
+
+
+
+
+local function create_chapter(time_pos)
+	if not time_pos then
+		time_pos = mp.get_property_number("time-pos")
+	end
+	verb("Creating chapter at ", time_pos)
+	local time_pos_osd = mp.get_property_osd("playback-time/full")
+	local curr_chapter = mp.get_property_number("chapter")
+	local chapter_count = mp.get_property_number("chapter-list/count")
+	local all_chapters = mp.get_property_native("chapter-list")
+
+	if chapter_count == 0 then
+		all_chapters[1] = {
+			title = "",
+			time = time_pos
+		}
+		curr_chapter = 0
+	else
+		for i = chapter_count, curr_chapter + 2, -1 do
+			all_chapters[i + 1] = all_chapters[i]
+		end
+		all_chapters[curr_chapter+2] = {
+			title = "",
+			time = time_pos
+		}
+	end
+	mp.set_property_native("chapter-list", all_chapters)
+	--mp.set_property_number("chapter", curr_chapter+1)
+	time_pos = nil
+
+end;mp.add_key_binding("n", "createChapter", create_chapter)
+
+
+
+
+local function deletechapters()
+	mp.set_property_native("chapter-list", {})
+end;mp.add_key_binding("Ctrl+D", "deletechapters", deletechapters)
+
+
+
+
+local function reloadTrs()
+	-- rebuilds chapters from Trs
+
+	mp.set_property_native("chapter-list", {})
+		-- Delete all chapters
+	
+	for i, val in pairs(Trs) do
+		if val['path'] == mp.get_property('stream-open-filename') then
+
+			print(i)
+
+			if Trs[i]['start'] then
+				create_chapter(Trs[i]['start'])
+			else
+				verb("not creating start chapter for index ", i)
+
+			end
+
+			if Trs[i]['fin'] then
+				create_chapter(Trs[i]['fin'])
+			else
+				verb("Not creating fin chapter for index ", i)
+			end
+		end
+	end
+end;
+	mp.add_key_binding("R", "reloadTrs", reloadTrs)
+	mp.register_event("file-loaded", reloadTrs)
+
+
+
+
+local function incrIndex()
+
+	if #Trs < 2 and Trs[Index+1] == nil then
+		notify(dur, "You only have one starter index,\nstart making a second index before cycling through them.")
+		return
+	end
+
+	if Trs[Index+1] == nil then
+		Index = 1 -- Looping through
+		notify(dur, "[c] (Looping) Increased index back down to ".. Index)
+	else
+		Index = Index + 1
+		notify(dur, "[C] Increased index to ".. Index)
+	end
+end;mp.add_key_binding("C", "increaseIndex", incrIndex)
+
+
+
+
+local function decrIndex()
+	if #Trs < 2 then
+		notify(dur, "You only have one starter index,\nstart making a second index before cycling through them.")
+		return
+	end
+
+	if Trs[Index - 1] == nil then
+		Index = #Trs -- Looping through
+		notify(dur, "[c] (Looping) Lowered index back up to ".. Index)
+	else
+		Index = Index - 1
+		notify(dur, "[c] Lowered index to ".. Index)
+	end
+end;mp.add_key_binding("c", "decreaseIndex", decrIndex)
+
+
+
+
+local function showPoints()
+	local fps = mp.get_property('container-fps')
+
+	if verbose then
+		print(utils.format_json(Trs))		
+	end
+
+	if #Trs == 0 then
+		notify(dur, "No trimming points yet")
+		return
+	end
+
+	msg = "Trimming points:"
+	if #Trs > 11 and Osdwarn == false	then
+		notify(dur, "You have too much trimming points\n for it to fit on the OSD, check the console.")
+		Osdwarn = true
+		return
+		elseif #Trs > 11 and Osdwarn == true then
+			return
+	end
+	for ind, _ in ipairs(Trs) do
+		if Trs[ind].path ~= nil then
+			msg = msg .. "\n" .. "[" .. ind .. "] " .. basename(Trs[ind].path)
+		end
+		if Trs[ind].start ~= nil then							       --/fps
+			msg = msg .. " : " .. string.sub(string.format(Trs[ind].start), 1, 4)
+		end
+		if Trs[ind].fin ~= nil then								     --/fps
+			msg = msg .. " - " .. string.sub(string.format(Trs[ind].fin), 1, 4)
+		end
+	end
+	print(msg)
+	mp.osd_message(msg, dur/500) -- 2x longer than normal dur, divide by / 1000 for same length
+end;mp.add_key_binding("Ctrl+p", "showPoints", showPoints)
+
+
+
+
+local function getIndex()
+	notify(dur, "[g] Selected index is ".. Index)
+end;mp.add_key_binding("Ctrl+g", "getCurrentIndex", getIndex)
+
+
+
+
+local function start()
+	local pos = mp.get_property_number('playback-time/full')
+	local fn = mp.get_property("stream-open-filename")
+	local curframe = mp.get_property_number('time-pos')
+
+	if string.match(fn,direcseparator) == nil then -- If running on command line stuff like 'mpv *.mp4' it doesn't provide full path
+		fn = mp.get_property("working-directory")..direcseparator..fn
+	end
+
+	if Trs[Index] == nil then Trs[Index] = {} end
+	Trs[Index]['start'] = curframe
+	Trs[Index]['path'] = fn
+
+	notify(dur, "[g] Set start point of index ["..Index.."] at ".. round(pos))
+
+	create_chapter();reloadTrs()
+
+end;mp.add_key_binding("g", "set-start", start)
+
+
+
+
+local function sof()
+	notify(dur, "[S] Setting index " .. Index .. " to 00:00:00 (start of file)")
+	if Trs[Index] == nil then Trs[Index] = {} end
+	Trs[Index]['start'] = 0
+end;mp.add_key_binding("G", "set-sof", sof)
+
+
+
+
+local function fin()
+
+	local pos = mp.get_property_number('playback-time/full')
+	local fn = mp.get_property("stream-open-filename")
+	local curframe = mp.get_property_number('time-pos')
+	if Trs[Index] == nil then Trs[Index] = {} end
+	Trs[Index]['fin'] = curframe
+	if string.match(fn,direcseparator) == nil then -- If running on command line stuff like *.mp4 it doesn't provide full path
+		fn = mp.get_property("working-directory")..direcseparator..fn
+	end
+
+	if Trs[Index]['start'] == nil and Trs[Index-1]['start'] ~= nil then
+		notify(dur, "[g] You need to set a start point first.")
+		return nil
+		-- Trs[Index-1]['fin'] = curframe
+	end
+
+	Trs[Index]['path'] = fn
+
+	notify(dur, "[h] Set end point of index ["..Index.."] at ".. round(pos))
+
+	if Trs[Index + 1] == nil then
+		Trs[Index + 1] = {}
+	end
+
+	if Trs[Index + 1]['start'] == nil and Trs[Index + 1]['fin'] == nil then -- Only step up if it's the last index
+		Index = Index + 1													 -- Else it means the user has went back down on an older index
+	end
+
+	create_chapter();reloadTrs()
+
+end;mp.add_key_binding("h", "set-fin", fin)
+
+
+
+
+local function eof()
+
+	if Trs[Index]['start'] == nil then
+		notify(dur, "[g] You need to set a start point first.")
+		return
+	end
+
+	local framecount = mp.get_property('duration')
+	notify(dur, "[H] Set end point of index ["..Index.."] to ".. framecount .. " (End of file)")
+	Trs[Index]['fin'] = framecount
+end;mp.add_key_binding("H", "set-eof", eof)
+
+
+
+
+local function render()
+	Trs[#Trs] = nil -- beautiful syntax to remove last object
+	Cmd = {args={'sm','-json', utils.format_json(Trs)}}
+	if mode == 'split' then table.insert(Cmd.args,'-split')
+	elseif mode == 'trim' then table.insert(Cmd.args,'-trim')
+	else print('UKNOWN MODE: ' .. mode) end
+	if verbose == true then
+		table.insert(Cmd.args,'-verbose')
+		local command = ''
+		for _, k in pairs(Cmd.args) do -- awful for loop to join array to string
+			command = command .. k .. ' '
+		end
+		print('COMMAND: ' .. command)
+	end
+	utils.subprocess_detached(Cmd)
+	mp.commandv('quit')
+end;mp.add_key_binding("Ctrl+r", "exportSLC", render)
+
+
+
+
+local function cycleModes()
+	if mode == 'trim' then
+		notify(dur, "[k] SPLIT MODE: Separating cuts into separate files")
+		mode = 'split'
+	elseif mode == 'split' then
+		notify(dur, "[k] TRIM MODE: Joining each videos' cuts")
+		mode = 'trim'
+	end
+end;mp.add_key_binding("k", "toggleSLCexportModes", cycleModes)
+
+
+
+
+local function toggleVerb()
+	if verbose == true then
+		notify(dur, "[Ctrl+v] toggled off Verbose")
+		verbose = false
+	elseif verbose == false then
+		notify(dur, "[Ctrl+v] toggled on Verbose")
+		verbose = true
+	end
+end;mp.add_key_binding("Ctrl+v", "toggleSLCverbose", toggleVerb)
